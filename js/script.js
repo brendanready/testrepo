6f277aa9c46f760d95ae53e0dd2ef5853507f09c
Brendan Ready <brendanbready@gmail.com>
2022-07-14 05:51:40 -0700

    Add file

---
+++ js/script.js
@@ -0,0 +1,174 @@
+/*
+        Brendan Ready
+        Assignment 01
+*/
+
+// STEP 1
+/*
+let someMonth;
+function theMonth();
+let currentMonth
+let summerMonth;
+let myLibraryFunction
+
+*/
+
+//STEP 2
+/*
+4.9                             // numeric literal expression
+"Hello World"                   // string literal expression
+true                            // Boolean literal expression
+null                            // null literal expression
+*/
+
+//STEP 3
+/*
+let totalPoints = 2 * (6 + 5) - 2;
+let userMsg = "Hello" + firstName + lastName;
+*/
+
+//STEP 4
+/*
+let FirstName, LastName, Address, City, State, ZipCode, YourAge, ReferralSource, MayWeContactYou;
+*/
+
+//STEP 5
+/*
+
+let LastName;
+let ZipCode;
+let MayWeContactYou;
+LastName = "Ready";
+ZipCode = 92027;
+MayWeContactYou = true;
+
+let LastName = "Ready";
+let ZipCode = 92027;
+let MayWeContactYou = true;
+
+let LastName = "Ready", ZipCode = 92027, MayWeContactYou = true;
+*/
+
+//STEP 6
+/*
+let numString = 549 + " Park Ave.";
+console.log(numString);
+*/
+
+//STEP 7
+/*
+
+let booString = true + " May We Contact You?";
+console.log(booString);
+
+let NumbBoo = 6 + false;
+console.log(NumbBoo);
+*/
+
+//STEP 8
+/*
+// No, it's not valid.  You must add a backslash to "I'm" to make it "I\'m" in order to denote that the quote in the string is meant to be taken as a literal 
+character, not as an end-of-string terminator.
+
+let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+console.log(someString);
+*/
+
+//STEP 9
+/*
+let x = null;
+console.log(x);                                  //null value returned
+let y;
+console.log(y);                                 //undefined value returned
+*/
+
+//STEP 10
+/*
+console.log(typeof "Welcome to Programming 101");       //returns string
+console.log(typeof 8);                                  //returns number
+console.log(typeof false);                              //returns Boolean
+console.log(typeof {city:'San Diego', zip:91942});      //returns object
+console.log(typeof ZipCode);                            //returns undefined
+*/
+
+//STEP 11
+/*
+alert("Hello " + "Brendan Ready" + ", welcome to the JavaScript class!");
+*/
+
+//STEP 12
+/*
+let name = "Brendan Ready";
+alert("Hello " + name + ", welcome to the JavaScript class!");
+*/
+
+//STEP 13
+/*
+let name = "Brendan Ready";
+let course = "JavaScript";
+alert("Hello " + name + ", welcome to the " + course + " class!");
+*/
+
+//STEP 14
+/*
+let name = "Brendan Ready";
+let course = "JavaScript";
+alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+*/
+
+//STEP 15
+/*
+let name = prompt('What is your name?');
+let course = "JavaScript";
+alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+*/
+
+//STEP 16
+/*
+let name = prompt('What is your name?');
+let course = prompt('What class are you taking?');
+alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+*/
+
+//STEP 17
+/*
+let x = 10;
+let y = 20;
+console.log(x + y);
+*/
+
+//STEP 18
+/*
+let x = 20;
+x += 20;
+console.log(x);
+*/
+
+//STEP 19
+/*
+let x = 20;
+x *= 5;
+console.log(x);
+*/
+
+//STEP 20
+/*
+let x = 20 % 3;
+x /= 1;
+console.log(x);
+*/
+
+//STEP 21
+/*
+let x = 2;
+let y = 1;
+console.log(x > y || x < y);       //evaluates to true
+*/
+
+//STEP 22
+/*
+var x = 3;
+var y = 10;
+var z = "3";
+console.log(x === z && y >= 9);     //evaluates to false
+*/
